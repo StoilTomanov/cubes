e38b5ec1ba2238dbd01ee01a0b7b3d9d9cff6941
StoilTomanov <stoiltomanov@gmail.com>
2022-03-11 13:08:11 +0200

    added checks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Cube';
+
+  public clearGrid(){
+    const arr = document.querySelectorAll<HTMLInputElement>('.grid');
+    arr.forEach((el)=>{
+      el.value = '';
+    });
+  }
 }
--- src/app/cube/cube.component.ts
+++ src/app/cube/cube.component.ts
@@ -8,24 +8,31 @@ import { Component, OnInit } from '@angular/core';
 export class CubeComponent implements OnInit {
   numberOfRows: number = 3;
   rows: HTMLDivElement[] = new Array(this.numberOfRows);
-  message: string = '';
+  message: string = 'Press Check button to run the check';
   includedNum: string[] = [];
-  
+
   constructor() { }
-  
+
   ngOnInit(): void {
   }
-  
+
   public readNums(): void {
     const arr = document.querySelectorAll<HTMLInputElement>('.grid');
+    let msg = document.getElementById('msg');
     arr.forEach((el: HTMLInputElement) => {
       if (this.includedNum.includes(el.value)) {
-        this.message = 'Numbers are not unique!'
+        this.message = 'Numbers are not unique!';
+        msg!.style.color = 'red';
         return;
-      } else{
+      } else {
         this.includedNum.push(el.value);
+        this.message = 'Success!';
+        msg!.style.color = 'green';
       }
-      this.message = 'Success!'
+    });
+
+    arr.forEach((el: HTMLInputElement) => {
+      el.value = '';
     });
   }
 }
