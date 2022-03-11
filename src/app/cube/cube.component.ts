c44fdfeead12fbe666257e57c713f07b44b8e8c8
StoilTomanov <stoiltomanov@gmail.com>
2022-03-11 12:36:59 +0200

    added functionality

--- src/app/cube/cube.component.ts
+++ src/app/cube/cube.component.ts
@@ -1,22 +1,31 @@
-import { Component, Injectable, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-cube',
   templateUrl: './cube.component.html',
   styleUrls: ['./cube.component.css']
 })
-@Injectable()
 export class CubeComponent implements OnInit {
-
+  numberOfRows: number = 3;
+  rows: HTMLDivElement[] = new Array(this.numberOfRows);
+  message: string = '';
+  includedNum: string[] = [];
+  
   constructor() { }
-
+  
   ngOnInit(): void {
   }
-
+  
   public readNums(): void {
-    const grid = document.getElementById('grid');
-    console.log(grid?.parentElement?.children[0]);
-
+    const arr = document.querySelectorAll<HTMLInputElement>('.grid');
+    arr.forEach((el: HTMLInputElement) => {
+      if (this.includedNum.includes(el.value)) {
+        this.message = 'Numbers are not unique!'
+        return;
+      } else{
+        this.includedNum.push(el.value);
+      }
+      this.message = 'Success!'
+    });
   }
-
 }
